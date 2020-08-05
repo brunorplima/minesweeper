765188d0d37c54fc6b0e2f12966f8af0ade37773
Bruno Lima <brunolima.cad@gmail.com>
2020-08-05 03:41:45 -0600

    added constants for the name of collections in firebase firestore

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,9 @@
-export const WON = 'won';
 
+
+export const WON = 'won';
 export const LOST = 'lost';
+
+
+export const EASY = 'easy';
+export const MEDIUM = 'medium';
+export const EXPERT = 'expert';
